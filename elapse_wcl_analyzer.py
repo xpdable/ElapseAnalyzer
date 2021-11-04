9b57d0c0d75808d3683015d574281a4a51abc300
Xiaopeng Liu <xiaopeng.liu@daimler.com>
2021-11-04 11:54:48 +0800

    output ranks of dps per encounter

--- elapse_wcl_analyzer.py
+++ elapse_wcl_analyzer.py
@@ -35,9 +35,9 @@ def gql_client(bearer_token:str) -> GraphqlClient:
 
 if __name__ == '__main__':
 
-# Get Single Report
-# Option 1 - via report link https://cn.classic.warcraftlogs.com/reports/rwG81bzxZvg3mDN9 or the code rwG81bzxZvg3mDN9 
-# TODO Option 2 - via guild code and report title
+    # Get Single Report
+    # Option 1 - via report link https://cn.classic.warcraftlogs.com/reports/rwG81bzxZvg3mDN9 or the code rwG81bzxZvg3mDN9 
+    # TODO Option 2 - via guild code and report title
 
     inputopts, args = getopt.getopt(sys.argv[1:],"h:l:gt")
     for o, val in inputopts:
@@ -59,35 +59,89 @@ if __name__ == '__main__':
     # Repor ID: e.g. rwG81bzxZvg3mDN9 
     report_code = wcl_report_link.split('/')[-1]
 
-    client = gql_client(_get_token())
+    client = gql_client(_get_token(None))
 
+    # Step 3 - Get Fights
     # Get Report All Fights
     # killType {Encounters=={Kills+Wipes},Kills==true,Wipes==false,Trash==null}
+    # translate:true aligned as English
     all_fights_query = """
         query {
-        reportData {
-            report(code: "{}") {
-                fights(translate:false) {
-                    id
-                    name
-                    kill
+            reportData {
+                report(code: "%s") {
+                    fights(translate:true) {
+                        id
+                        name
+                        kill
+                        startTime
+                        endTime
+                    }
                 }
             }
         }
-    }
-    """.format(report_code)
+    """%report_code
+
     all_fights_data = client.execute(query=all_fights_query)
 
+    # {'data': {'reportData': {'report': {'fights': []
+    # print(all_fights_data)
+    all_fights_list = all_fights_data['data']['reportData']['report']['fights']
+    kills_fights_id_list = list()
+    # 3.1 Select Fights
+    for f in all_fights_list:
+        if f['kill'] is True:
+            kills_fights_id_list.append(f)
+    #print(kills_fights_id_list)
 
-# Step 2 - Get Fight Report
+    # Step 4 - Get Player Fight Ranks
+    rankings_query="""
+        query {
+            reportData {
+                report(code: "%s") {
+                    rankings
+                }
+            }
+        }
+    """%report_code
 
-# Step 3 - Get Fights
+    all_rankings_data = client.execute(query=rankings_query)
+    # {'data': {'reportData': {'report': {'rankings': json
+    all_rankings_list = all_rankings_data['data']['reportData']['report']['rankings']['data']
+    #print(all_rankings_json)
 
-# 3.1 Select Fights
+    #DPS Ranks model
+    # [{boss1:[{name:player1,class:pal,parse:87,item:66},{name:player1,class:pal,parse:87,item:66}]},{boss2:[]}]
+    elapse_ranking = list()
+
+    #Get DPS fights ranks
+    kills_fights_id_arr = list()
+    for i in kills_fights_id_list:
+        kills_fights_id_arr.append(i['id'])
+    for r in all_rankings_list:
+        if r['fightID'] in kills_fights_id_arr:
+            boss = r['encounter']['name']
+            dps_list = r['roles']['dps']['characters']
+            boss_dps_list = list()
+            for d in dps_list:
+                player_name = d['name']
+                player_class = d['class']
+                player_parse = d['rankPercent']
+                player_bracket = d['bracketPercent']
+                player_dict = dict()
+                player_dict['name'] = player_name
+                player_dict['class'] = player_class
+                player_dict['parse'] = player_parse
+                player_dict['bracket'] = player_bracket
+                boss_dps_list.append(player_dict)
+            elapse_ranking.append({"encounter":boss,"ranks":boss_dps_list})
+
+    print(elapse_ranking)
 
 
 
-# Step 4 - Get Player Fight Ranks
+
+
+
 
 
 # Synchronous request
