960b8fb4ef76d63590752ac87b85898f722b7f20
Xiaopeng Liu <xiaopeng.liu@daimler.com>
2021-11-04 17:45:16 +0800

    feature 1, GA in console

--- elapse_wcl_analyzer.py
+++ elapse_wcl_analyzer.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os, sys, getopt
+import csv
 from python_graphql_client import GraphqlClient
 
 TOKEN_URL = 'https://www.warcraftlogs.com/oauth/token'
@@ -110,17 +111,20 @@ if __name__ == '__main__':
     #print(all_rankings_json)
 
     #DPS Ranks model
-    # [{boss1:[{name:player1,class:pal,parse:87,item:66},{name:player1,class:pal,parse:87,item:66}]},{boss2:[]}]
-    elapse_ranking = list()
+    # [{boss1:[{name:player1,class:pal,parse:87,item:66},{name:player1,class:pal,parse:87,item:66}]},...]
+    elapse_ranking = dict()
 
     #Get DPS fights ranks
     kills_fights_id_arr = list()
+    dps_players = list()
+    boss_name = list()
     for i in kills_fights_id_list:
         kills_fights_id_arr.append(i['id'])
     for r in all_rankings_list:
         if r['fightID'] in kills_fights_id_arr:
             boss = r['encounter']['name']
             dps_list = r['roles']['dps']['characters']
+            boss_name.append(boss)
             boss_dps_list = list()
             for d in dps_list:
                 player_name = d['name']
@@ -129,13 +133,98 @@ if __name__ == '__main__':
                 player_bracket = d['bracketPercent']
                 player_dict = dict()
                 player_dict['name'] = player_name
+                dps_players.append(player_name)
                 player_dict['class'] = player_class
                 player_dict['parse'] = player_parse
                 player_dict['bracket'] = player_bracket
                 boss_dps_list.append(player_dict)
-            elapse_ranking.append({"encounter":boss,"ranks":boss_dps_list})
+            elapse_ranking[boss]=boss_dps_list
+    
+    #print(json.dumps(elapse_ranking))
+    # Should form the metrix here other than
+    # build a metrix col ->           boss1, boss2 avg
+    #                row -> player1   p+b    p+b   player1-avg
+    #                       player2   
+    #                       boss-avg  b-avg        benchmark-avg
+    # if player does not show in bossX, use 0
+    dps_players_global = list(set(dps_players))
+    boss_name = list(set(boss_name))
 
-    print(elapse_ranking)
+    # Style 1 Player per Encounters
+    # 止戈之战
+    # |-Hydross the Unstable 193
+    # |-The Lurker Below 198
+    # |-Leotheras the Blind 188
+    # |-Fathom-Lord Karathress 194
+    # |-Morogrim Tidewalker 185
+    # |-Lady Vashj 186
+    # |-A'lar 196
+    # |-Void Reaver 191
+    # |-High Astromancer Solarian 141
+    # |-Kael'thas Sunstrider 164
+    for p in dps_players_global:
+        print(p)
+        for encounter, dps_player_list in elapse_ranking.items():
+            dps_participants = list()
+            for dps in dps_player_list:
+                dps_participants.append(dps['name']) 
+            if p in dps_participants:
+                for pp in dps_player_list:
+                    if p == pp['name']:
+                        parse = pp['parse']
+                        bracket = pp['bracket']
+                score = parse + bracket
+            else:
+                score = 0
+            # print("|-{} {}".format(encounter,score))
+
+
+    # Style 2 Encounter per player
+    # Kael'thas Sunstrider
+    # |-同舟共济 176
+    # |-吃素的尼姑 108
+    # |-黑鼻子 183
+    # |-小小星痕 150
+    # |-蓝条狂暴战 139
+    # |-妖应封光 160
+    # |-老衲不吃肥肉 155
+    # |-Richturteil 0
+    # |-Lolit 165
+    # |-神之无之 139
+    # |-莉莉科林斯 30
+    # |-荀文若 75
+    # |-鲁德彪 0
+    # |-Galgadòt 18
+    # |-雾染霜华 0
+    # |-强彡森 111
+    # |-神卡布 0
+    # |-屁神久我 71
+    # |-止戈之战 164
+    # |-慕蔺 18
+    # for encounter, dps_player_list in elapse_ranking.items():
+    #     print(encounter)
+    #     dps_participants = list()
+    #     for dps in dps_player_list:
+    #         dps_participants.append(dps['name'])
+    #     for p in dps_players_global:
+    #         if p in dps_participants:
+    #             for pp in dps_player_list:
+    #                 if p == pp['name']:
+    #                     parse = pp['parse']
+    #                     bracket = pp['bracket']
+    #             score = parse + bracket
+    #         else:
+    #             score = 0
+    #         print("|-{} {}".format(p,score))
+
+            
+
+    #print(score)
+
+    # 4.1 Print CSV
+    # with open('boss_dps_ranking.csv', 'w') as f: 
+    #     w = csv.writer(f)
+    #     w.writerow(elapse_ranking)
 
 
 
